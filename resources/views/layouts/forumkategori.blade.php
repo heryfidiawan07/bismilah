7e392de914f138b1a16fe1405524c027d86b672b
one07 <heryfidiawan07@gmail.com>
2017-04-29 21:37:52 +0700

    mod btn forum

--- resources/views/layouts/forumkategori.blade.php
+++ resources/views/layouts/forumkategori.blade.php
@@ -1,4 +1,4 @@
-<a href="#" type="button" class="" data-toggle="modal" data-target="#forumModal">
+<a href="#" type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#forumModal">
     Forum Kategori
 </a>
 <!-- Modal -->
@@ -7,8 +7,8 @@
     <div class="modal-content">
 
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&#60; close</span></button>
-        <h4 class="modal-title" id="myModalLabel" style="color: white;">Kategori Forum</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&#62; close</span></button>
+        <h4 class="modal-title" id="myModalLabel" style="color: white;">Kategori</h4>
       </div>
 
       <div class="modal-body">
