8d46aa99b340465aad5b2ffe1bd5f509e73850c4
one07 <heryfidiawan07@gmail.com>
2017-05-07 08:10:48 +0700

    mod share button

--- resources/views/layouts/articleshare.blade.php
+++ resources/views/layouts/articleshare.blade.php
@@ -2,8 +2,8 @@
 
 	<b>Share : </b>
 	
-	<a href="https://www.facebook.com/sharer/sharer.php?u=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-facebook"> Facebook</a>
-	<a href="https://twitter.com/intent/tweet?url=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-twitter"> Twitter</a>
-	<a href="https://plus.google.com/share?url=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-google"> Google+</a>
+	<a href="https://www.facebook.com/sharer/sharer.php?u=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-facebook"></a>
+	<a href="https://twitter.com/intent/tweet?url=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-twitter"></a>
+	<a href="https://plus.google.com/share?url=http://kampusmobil.com/{{$article->brand->slug}}/{{$article->slug}}" target="_blank" class="fa fa-google"></a>
 
 </div>
