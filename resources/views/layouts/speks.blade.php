e09c4f294046d6f358bbcb1100cecd759de58d79
one07 <heryfidiawan07@gmail.com>
2017-05-07 08:00:18 +0700

    mod model to slug in spek index

--- resources/views/layouts/speks.blade.php
+++ resources/views/layouts/speks.blade.php
@@ -4,7 +4,7 @@
 		<div class="panel panel-default">
 		<div class="panel-body">
 			<div class="col-sm-5">
-				<a href="/spesifikasi/{{$spek->mobil->model}}/{{$spek->slug}}">
+				<a href="/spesifikasi/{{$spek->mobil->slug}}/{{$spek->slug}}">
 					<img src="https://lh3.googleusercontent.com/-WL0ZJvL2woo/WQBwHGJsPdI/AAAAAAAAAkg/sehLYuEPH5MnOCIo06PD-mx6V2xLa8SQwCHM/s200/%255BUNSET%255D" class="img-responsive" alt="spesification" id="imgS">
 				</a>
 			</div>
